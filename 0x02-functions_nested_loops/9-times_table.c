25405f61804ab535afcd6f31331465ddd9e18f79
ramzi2 <ramzi.badaoui@gmail.com>
2023-03-21 12:10:35 -0700

    a + b

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,23 +13,23 @@ void times_table(void)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-		t = i * j;
+	t = i * j;
 	if (t > 9)
 	{
-		u = t % 10;
-		d = (t - u) / 10;
-		_putchar(44);
-		_putchar(32);
-		_putchar(d + '0');
-		_putchar(u + '0');
+	u = t % 10;
+	d = (t - u) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(d + '0');
+	_putchar(u + '0');
 	}
 	else
 	{
 	if (j != 0)
 	{
-		_putchar(44);
-		_putchar(32);
-		_putchar(32);
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
 	}
 	_putchar(t + '0');
 	}
