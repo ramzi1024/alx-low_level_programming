776c08c235c8b7b8301e8cd0d2da5fe149420aa1
ramzi2 <ramzi.badaoui@gmail.com>
2023-04-07 05:09:41 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints
+ * @argc: argument
+ * @argv: argument
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1 = 0, n = 0;
+
+	if (argc == 3)
+	{
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	printf("%d\n", n1 * n2);
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+	return (0);
+}
